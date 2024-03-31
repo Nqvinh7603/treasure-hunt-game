f9090a7faf9e21a59976b70808b0bcf67554c289
Nguyễn Quang Vinh <139441823+Nqvinh7603@users.noreply.github.com>
2024-03-31 11:02:03 +0700

    set color title top10

--- src/main/java/com/treasurehuntgame/Top10.java
+++ src/main/java/com/treasurehuntgame/Top10.java
@@ -13,7 +13,107 @@ import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
+//
+//public class Top10 extends JDialog {
+//    private JComboBox<String> cb;
+//    String s;
+//    JLabel[] top = new JLabel[11];
+//
+//    public Top10() {
+//        setSize(500, 550);
+//        setTitle("TOP 10");
+//        JPanel p = new JPanel();
+//        p.setLayout(null);
+//        p.setBounds(0, 0, 500, 550);
+//        p.setBackground(new Color(34, 177, 76));
+//        add(p);
+//
+//        setLayout(null);
+//        String[] md = {"VERY EASY", "EASY", "MEDIUM", "HARD", "VERY HARD"};
+//        JLabel lbl = new JLabel("CHOOSE LEVEL");
+//        lbl.setBounds(70, 0, 400, 70);
+//        lbl.setFont(new Font("Tahoma", Font.BOLD, 30));
+//        lbl.setForeground(Color.RED);
+//        p.add(lbl);
+//
+//        cb = new JComboBox<>(md);
+//        cb.setBounds(140, 67, 180, 50);
+//        cb.setFont(new Font("Tahoma", Font.BOLD, 20));
+//        cb.setForeground(Color.BLUE);
+//        cb.setBackground(new Color(255, 255, 153)); // Chỉnh màu nền của JComboBox
+//        p.add(cb);
+//        s = "src/main/java/com/treasurehuntgame/data_top10/top10_1.txt";
+//
+//        String[] ten = new String[11];
+//        String[] diem = new String[11];
+//
+//        try {
+//            FileReader fr = new FileReader(s);
+//            BufferedReader br = new BufferedReader(fr);
+//            String line = "";
+//            int i = 0;
+//            while (++i <= 10) {
+//                line = br.readLine();
+//                ten[i] = new String(line);
+//                line = br.readLine();
+//                diem[i] = new String(line);
+//            }
+//            br.close();
+//            fr.close();
+//        } catch (Exception ex) {
+//        }
+//        for (int i = 1; i <= 10; i++) {
+//            String ss = "Top " + i + ":   " + ten[i] + "                                              " + diem[i] + "%";
+//            top[i] = new JLabel(ss.substring(0, 37) + ss.substring(ss.length() - 5, ss.length()));
+//            top[i].setForeground(Color.RED);
+//            top[i].setFont(new Font("Tahoma", Font.BOLD, 20));
+//            top[i].setBounds(50, 25 * i + 170, 400, 23);
+//            p.add(top[i]);
+//        }
+//
+//        cb.addItemListener(new ItemListener() {
+//            @Override
+//            public void itemStateChanged(ItemEvent e) {
+//                int cbx = cb.getSelectedIndex() + 1;
+//                s = "src/main/java/com/treasurehuntgame/data_top10/top10_" + cbx + ".txt";
+//                try {
+//                    FileReader fr = new FileReader(s);
+//                    BufferedReader br = new BufferedReader(fr);
+//                    String line = "";
+//                    int i = 0;
+//                    while (++i <= 10) {
+//                        line = br.readLine();
+//                        ten[i] = new String(line);
+//                        line = br.readLine();
+//                        diem[i] = new String(line);
+//                    }
+//                    br.close();
+//                    fr.close();
+//                } catch (Exception ex) {
+//                }
+//                for (int i = 1; i <= 10; i++) {
+//                    String ss = "Top " + i + ":   " + ten[i] + "                                               " + diem[i] + "%";
+//                    top[i].setText(ss.substring(0, 37) + ss.substring(ss.length() - 5, ss.length()));
+//                }
+//            }
+//        });
+//
+//        JButton button = new JButton("CLOSE");
+//        button.setBounds(330, 450, 150, 50);
+//        button.setFont(new Font("Tahoma", Font.BOLD, 30));
+//        button.setForeground(Color.RED);
+//        p.add(button);
+//
+//        setBackground(Color.YELLOW);
+//        setLocationRelativeTo(null);
+//
+//        button.addActionListener(new ActionListener() {
+//            public void actionPerformed(ActionEvent e) {
+//                setVisible(false);
+//            }
+//        });
+//    }
+//}
 public class Top10 extends JDialog {
     private JComboBox<String> cb;
     String s;
@@ -31,10 +131,17 @@ public class Top10 extends JDialog {
         setLayout(null);
         String[] md = {"VERY EASY", "EASY", "MEDIUM", "HARD", "VERY HARD"};
         JLabel lbl = new JLabel("CHOOSE LEVEL");
-        lbl.setBounds(70, 0, 400, 70);
+        lbl.setBounds(120, 0, 400, 70);
         lbl.setFont(new Font("Tahoma", Font.BOLD, 30));
         lbl.setForeground(Color.RED);
         p.add(lbl);
+
+        // Thêm chữ "Top 10" vào cửa sổ
+        JLabel top10Label = new JLabel("Top 10");
+        top10Label.setBounds(160, 130, 400, 40);
+        top10Label.setFont(new Font("Tahoma", Font.BOLD, 30));
+        top10Label.setForeground(Color.RED);
+        p.add(top10Label);
 
         cb = new JComboBox<>(md);
         cb.setBounds(140, 67, 180, 50);
@@ -65,7 +172,7 @@ public class Top10 extends JDialog {
         for (int i = 1; i <= 10; i++) {
             String ss = "Top " + i + ":   " + ten[i] + "                                              " + diem[i] + "%";
             top[i] = new JLabel(ss.substring(0, 37) + ss.substring(ss.length() - 5, ss.length()));
-            top[i].setForeground(Color.RED);
+            top[i].setForeground(Color.BLUE);
             top[i].setFont(new Font("Tahoma", Font.BOLD, 20));
             top[i].setBounds(50, 25 * i + 170, 400, 23);
             p.add(top[i]);
