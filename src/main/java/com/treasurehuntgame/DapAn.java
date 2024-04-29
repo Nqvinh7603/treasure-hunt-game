24ab32fff16031709d84dddbf7f436543ee2bd2c
Nguyễn Quang Vinh <139441823+Nqvinh7603@users.noreply.github.com>
2024-04-29 20:10:43 +0700

    update

--- src/main/java/com/treasurehuntgame/DapAn.java
+++ src/main/java/com/treasurehuntgame/DapAn.java
@@ -2,7 +2,9 @@ package com.treasurehuntgame;
 
 
 import java.awt.HeadlessException;
+
 import static java.lang.Thread.sleep;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
@@ -10,142 +12,134 @@ import javax.swing.JFrame;
 
 
 /**
- *
  * @author Vinh
  */
-public class DapAn extends JFrame implements Runnable{
+public class DapAn extends JFrame implements Runnable {
+    public int diem, count;
     Game g;
-    int[] temp = new int [255];
-    public int diem,count;
-    
+    int[] temp = new int[255];
+
     public DapAn(Game gg) {
         this.g = gg;
-        setSize(700,700);
+        setSize(700, 700);
         setLocationRelativeTo(null);
-           for(int i=0;i<15;i++)
-            for(int j=0;j<15;j++){
-    
-                if(g.gt.a[i][j] == -1000000)
-                {   g.A[i][j].setText(""); 
+        for (int i = 0; i < 15; i++)
+            for (int j = 0; j < 15; j++) {
+
+                if (g.gt.a[i][j] == -1000000) {
+                    g.A[i][j].setText("");
                     g.A[i][j].setIcon(new ImageIcon(getClass().getResource("/com/treasurehuntgame/img/boom.PNG")));
-                } else
-                if(g.gt.a[i][j] == -500)
-                {   g.A[i][j].setText("");
+                } else if (g.gt.a[i][j] == -500) {
+                    g.A[i][j].setText("");
                     g.A[i][j].setIcon(new ImageIcon(getClass().getResource("/com/treasurehuntgame/img/khunglong.PNG")));
-                } else
-                if(g.gt.a[i][j] == -300)
-                {   g.A[i][j].setText(""); 
+                } else if (g.gt.a[i][j] == -300) {
+                    g.A[i][j].setText("");
                     g.A[i][j].setIcon(new ImageIcon(getClass().getResource("/com/treasurehuntgame/img/ho.PNG")));
-                } else
-                if(g.gt.a[i][j] == -100)
-                {   g.A[i][j].setText(""); 
+                } else if (g.gt.a[i][j] == -100) {
+                    g.A[i][j].setText("");
                     g.A[i][j].setIcon(new ImageIcon(getClass().getResource("/com/treasurehuntgame/img/chosoi.PNG")));
-                } 
-    }
+                }
+            }
         add(g);
         Thread t = new Thread(this);
-        
+
         t.start();
-       // g.thuchien(3,3);
+        // g.thuchien(3,3);
     }
 
     @Override
     public void run() {
         giai();
-    for(int i=count;i>=0;i--)
-    {
-        g.thuchien(temp[i]/15,temp[i]%15);
-        if(i!=count) g.thuchienlai(temp[i+1]/15,temp[i+1]%15);
-        try {
-            sleep(250);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(DapAn.class.getName()).log(Level.SEVERE, null, ex);
+        for (int i = count; i >= 0; i--) {
+            g.thuchien(temp[i] / 15, temp[i] % 15);
+            if (i != count) g.thuchienlai(temp[i + 1] / 15, temp[i + 1] % 15);
+            try {
+                sleep(250);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(DapAn.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
         }
-        
     }
-    }
-    
-    public void giai(){
+
+    public void giai() {
         int a[][] = new int[225][225];
-  
-        for(int i=0;i<225;i++)
-            for(int j=0;j<225;j++){
-                a[i][j]=-10000000;
+
+        for (int i = 0; i < 225; i++)
+            for (int j = 0; j < 225; j++) {
+                a[i][j] = -10000000;
             }
-        for(int i=0;i<15;i++)
-            for(int j=0;j<15;j++){
-                if(j<14) a[15*i+j][15*i+j+1]=g.gt.a[i][j+1];
-                if(i>0) a[15*i+j][15*(i-1)+j]=g.gt.a[i-1][j];
-                if(j>0) a[15*i+j][15*i+j-1]=g.gt.a[i][j-1];
-                if(i<14) a[15*i+j][15*(i+1)+j]=g.gt.a[i+1][j];
+        for (int i = 0; i < 15; i++)
+            for (int j = 0; j < 15; j++) {
+                if (j < 14) a[15 * i + j][15 * i + j + 1] = g.gt.a[i][j + 1];
+                if (i > 0) a[15 * i + j][15 * (i - 1) + j] = g.gt.a[i - 1][j];
+                if (j > 0) a[15 * i + j][15 * i + j - 1] = g.gt.a[i][j - 1];
+                if (i < 14) a[15 * i + j][15 * (i + 1) + j] = g.gt.a[i + 1][j];
             }
-            for(int i=0;i<225;i++)
-                for(int j=0;j<225;j++){
-                  a[i][j]=-a[i][j];
+        for (int i = 0; i < 225; i++)
+            for (int j = 0; j < 225; j++) {
+                a[i][j] = -a[i][j];
             }
-             
-   
-  int n=224;    
-  int MAXINT = 10000000;
-  int src =0, des = 224;
-int[] DanhDau = new int[n + 1];
-int[] Nhan = new int[n + 1];
-int[] Truoc = new int[n + 1];
-int  min, i;
-  
-for (i = 0; i <= n; i++)
-{
-    Nhan[i] = MAXINT;
-    DanhDau[i] = 0;
-    Truoc[i] = src;
-}
-Nhan[src] = 0;
-DanhDau[src] = 0;
+
+
+        int n = 224;
+        int MAXINT = 10000000;
+        int src = 0, des = 224;
+        int[] DanhDau = new int[n + 1];
+        int[] Nhan = new int[n + 1];
+        int[] Truoc = new int[n + 1];
+        int min, i;
+
+        for (i = 0; i <= n; i++) {
+            Nhan[i] = MAXINT;
+            DanhDau[i] = 0;
+            Truoc[i] = src;
+        }
+        Nhan[src] = 0;
+        DanhDau[src] = 0;
 //Dijkstra -> start
-int u;
-while (true){
-    min = MAXINT; u=-1;
-    for (int j = 0; j <= n; j++)
-        if (min > Nhan[j] && DanhDau[j] == 0)
-        {
-            min = Nhan[j];
-            u = j;
+        int u;
+        while (true) {
+            min = MAXINT;
+            u = -1;
+            for (int j = 0; j <= n; j++)
+                if (min > Nhan[j] && DanhDau[j] == 0) {
+                    min = Nhan[j];
+                    u = j;
+                }
+            if (u == -1 || u == n) break;
+
+            DanhDau[u] = 1;
+
+            for (int v = 0; v <= n; v++)
+                if (Nhan[v] > a[u][v] + Nhan[u] && DanhDau[v] == 0) {
+                    Nhan[v] = a[u][v] + Nhan[u];
+                    Truoc[v] = u;
+                }
         }
-    if(u==-1||u==n) break;
-    
-    DanhDau[u] = 1;
- 
-    for (int v = 0; v <= n; v++)
-        if (Nhan[v] > a[u][v] + Nhan[u] && DanhDau[v] == 0)
-        {
-            Nhan[v] = a[u][v] + Nhan[u];
-            Truoc[v] = u;
-        }
-}
 //disjkstra -> end
-             
+
 // Truy vết đường đi và in kết quả
 
-temp[0] = des;
-temp[1] = Truoc[des];
-i = Truoc[des];
-int count = 2;
-while (i != src)
-{
-    i = Truoc[i];
-    temp[count] = i;
-    count++;
-}
+        temp[0] = des;
+        temp[1] = Truoc[des];
+        i = Truoc[des];
+        int count = 2;
+        while (i != src) {
+            i = Truoc[i];
+            temp[count] = i;
+            count++;
+        }
 
-this.count = count-1;
+        this.count = count - 1;
 //for (i = count - 1; i >= 1; i--)
 //    System.out.println(temp[i] + " --> ");
 //    System.out.println(temp[0] + ": " + Nhan[des]);  
-    
-        setTitle("The least energy consumptiont: "+Nhan[des]);
-    this.diem=Nhan[des];
+
+        setTitle("The least energy consumptiont: " + Nhan[des]);
+        this.diem = Nhan[des];
 //for(i=0;i<225;i++)
-              
-      
+
+
     }
 }
